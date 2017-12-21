f5349d2862f735f28d8757d332ead68cd57ae1c5
Takanori Yanagitani <bruder@ezweb.ne.jp>
2017-12-21 19:13:20 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,54 @@
 (() => {
+
+  const requestAsync = (url, method, data) => new Promise(requestConsumer => {
+    const request = new XMLHttpRequest();
+    request.open(method || "GET", url, true);
+    request.addEventListener("readystatechange", () => {
+      const {
+        readyState,
+        status,
+      } = request || {};
+      if(4 != readyState) return;
+      if(200 != status) return;
+      requestConsumer(request);
+    }, false);
+    request.send(data);
+  });
+  
+  const iterateAsync = (iterator, consumerAsync) => new Promise(iterated => {
+    if(! iterator)      return iterated();
+    if(! iterator.next) return iterated();
+    if(! consumerAsync) return iterated();
+    const loop = {
+      const next = iterator.next();
+      if(! next)    return iterated();
+      if(next.done) return iterated();
+      consumerAsync(next.value).then(loop);
+    };
+    loop();
+  });
+  
+  iteratorAsync(
+    [
+      "https://unpkg.com/react@16/umd/react.development.js",
+      "https://unpkg.com/react-dom@16/umd/react-dom.development.js",
+      "https://unpkg.com/babel-standalone@6.15.0/babel.min.js",
+    ],
+    url => new Promise(iterated => {
+      const cached = localStorage.getItem(url);
+      new Promise(sourceConsumer => {
+        if(cached) return sourceConsumer(cached);
+      })
+      .then(source => {
+        (new Function(source))();
+        iterated();
+      });
+    })
+  ).then(loaded => {
+    alert(window.React);
+  });
+  
+
+  
 })()
 ;
